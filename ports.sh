b8e3324d59b0bfd69fcc7a2628c66e26fa5016cb
Cazu291 <eu.bailliet@gmail.com>
2025-11-12 19:40:36 +0200

    visual update

--- ports.sh
+++ ports.sh
@@ -4,29 +4,58 @@ separator="================================================================"
 
 if [ -z $1 ]; then
         if [ -z $box ]; then
-                echo "you have to either set the variable box or provide an ip so i set it for you"
+                echo "Please either set the variable \$box or provide an ip so i set it for you"
         fi
 else
         # assigning value and pretty print
         export box=$1
         echo $separator
-        echo "__"
+        echo "--"
         echo "||"
         echo "||target is: $box"
         echo "||"
-        echo "__"
+        echo "--"
         echo $separator
 
         # basic nmap
+	echo "-"
+	echo "|"
+	echo "| Running a basic nmap scan on the target"
+	echo "|"
+	echo "| $ nmap -sV $box -o nmap.scan"
+	echo "|"
+	echo "-"
+	echo ""
         nmap -sV $box -o nmap.scan
 
         # advanced nmap
+	echo "-"
+	echo "|"
+	echo "| Running an extended nmap scan on $box"
+	echo "|"
+	echo "| $ nmap -A $box -o nmap_extended.scan"
+	echo "|"
+	echo "-"
         nmap -A $box -o nmap_extended.scan
 
         # Syn stealth nmap
         # needs the sudo to be started
+	echo "-"
+        echo "|"
+        echo "| Running a Syn Stealth nmap scan on $box"
+        echo "|"
+        echo "| $ sudo nmap -sS $box -o nmap_stealth.scan"
+        echo "|"
+        echo "-"
         sudo nmap -sS $box -o nmap_stealth.scan
 
         # UDP nmap
+        echo "-"
+        echo "|"
+        echo "| Running an UDP nmap scan on $box"
+        echo "|"
+        echo "| $ sudo nmap -sU $box -o nmap_udp.scan"
+        echo "|"
+        echo "-"
         nmap -sU $box -o nmap_udp.scan
 fi
