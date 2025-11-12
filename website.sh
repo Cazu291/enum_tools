27c8226750a293247c488eff29d370153f62ca9f
Cazu291 <eu.bailliet@gmail.com>
2025-11-12 19:32:32 +0200

    first version of website scanner

---
+++ website.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+separator="================================================================"
+
+if [ -z $1 ]; then
+        echo "command usage is: ports.sh <target> [<dirs_wordlist> <subs_wordlist>]"
+else
+        dirs_wordlist=/usr/share/wordlists/dirb/big.txt
+        subs_wordlist=/usr/share/wordlists/dirb/subs/n0kovo_subdomains/n0kovo_subdomains_medium.txt
+
+        # pretty print
+        target=$1
+        echo separator
+        echo "--"
+        echo "||"
+        echo "|| Scans will now begin on directories and subdomains"
+        echo "||"
+        echo "--"
+        echo ""
+        echo separator
+
+        # scans for dirs
+        echo "-"
+        echo "|"
+        echo "| Now scanning for directories and web pages on the main page"
+        echo "|"
+        echo "$ ffuf -u http://$box/FUZZ -w $dirs_wordlist -o dirs.scan"
+        echo "|"
+        echo "-"
+        echo ""
+        ffuf -u http://$box/FUZZ -w $dirs_wordlist -o dirs.scan
+
+        # scans for subdomains
+        echo "-"
+        echo "|"
+        echo "| Now scanning for subdirectories starting from the main page"
+        echo "|"
+        echo "$ ffuf -u http://FUZZ.$box -w $subs_wordlist -o subs.scan"
+        echo "|"
+        echo "-"
+        echo ""
+        ffuf -u http://FUZZ.$box -w $subs_wordlist -o subs.scan
+
+fi
