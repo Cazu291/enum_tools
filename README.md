e843491b8848c70b51448d05755459bd75e3a9cb
Cazu291 <eu.bailliet@gmail.com>
2025-11-13 23:13:07 +0200

    options are now available for target, url, dirlist, sublist and filelist, --default is here but does nothing, readme was added

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # enum_tools
+![[image.png]]
+
+This tool was designed to run a very simple set of commands for scanning a machine, especially for a htb machine, as they usually contain a website to use for privesc.
+
+Quick command is
+```
+./aio.sh -t <ip>
+```
+
+If it is impossible to connect to the website without the use of the domain name, use the `-u` option:
+```
+./aio.sh -t <ip> -u <domain_name.com>
+```
+
+For a more complete control of the wordlists used for directories, files and subdomains scanning, you can respectively use the options `-d`, `-f` and `-s`
+```
+./aio.sh -t <ip> -d <dirs_list> -f <files_list> -s <subs_list>
+```
+
+All scans outputs are written in the files `nmap.scan`, `nma-all-ports.scan`, `nmap-udp.scan`, `dirs.scan` and `subs.scan`. The options to modify those outputs files are not yet available.
--- aio.sh
+++ aio.sh
@@ -11,9 +11,97 @@ read -r -d '' ascii_art << 'EOF'
 EOF
 
 printf "%s\n" "$ascii_art"
+echo ""
+echo ""
+echo "Thank you for using this tool"
+echo "credit goes to nmap and ffuf for being so useful"
+echo " - TheLastOfEugenes"
+
 
 header_separator="===================================================================================="
 command_separator="================================================================"
+
+# Error message function
+error_message () {
+	echo ""
+	echo "usage: aio.sh ([<flag> [<value>]]*|--target <target>)"
+	echo "  Options:"
+	echo "    -t|--target <target>: specifies the targeted ip, required"
+	echo "    -d|--dirlist <list>	: change the file for the directories fuzzing, default is	/usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-directories-lowercase.txt"
+	echo "    -f|--filelist <list>: change the file for the files fuzzing, default is		/usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-files-lowercase.txt"
+	echo "    -s|--sublist <list>	: change the file for the subs fuzzing, default is		/usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-110000.txt"
+	echo "    -u|--url <url>	: sets the domain name/url for the web scans, for exmple editor.htb"
+	echo ""
+}
+
+
+# Source - https://stackoverflow.com/a
+# gets arguments
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -t|--target)
+      target="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -d|--dirlist)
+      dirlist="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -f|--filelist)
+      filelist="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -s|--sublist)
+      sublist="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -u|--url)
+      url="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --default)
+      DEFAULT=YES
+      shift # past argument
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+if [ -z "$target" ]; then
+	error_message
+	exit 1
+fi
+
+set -- "${POSITIONAL_ARGS[@]}"
+
+if [ -z "$url" ]; then
+	url="$target"
+fi
+
+if [ -z "$filelist" ]; then
+	filelist="/usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-files-lowercase.txt"
+fi
+if [ -z "$dirlist" ]; then
+	dirlist="/usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-directories-lowercase.txt"
+fi
+if [ -z "$sublist" ]; then
+	sublist="/usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-110000.txt"
+fi
 
 print_header () {
 	echo $header_separator
@@ -39,40 +127,38 @@ run_command () {
 	echo ""
 }
 
-run=false
-
-if [ -z $1 ]; then
-        echo "Command usage is: aio.sh <target> [<website>]"
-else
-	target=$1
-	web_scans=false;
-	if [ -z $3 ]; then
-		echo "website not specified, dirs and subs scans will be skipped"
-	else
-		website=$3
-		web_scans=true
-	fi
-
-	files_wordlist=/usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-files-lowercase.txt
-        dirs_wordlist=/usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-directories-lowercase.txt
-        subs_wordlist=/usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-110000.txt
+main () {
 
 	echo $banner
 	echo ""
 	echo ""
 
+	echo $command_separator
+	echo "|| set parameters are:"
+	echo "||"
+	echo "|| target: $target"
+	echo "|| url: $url"
+	echo "|| dirlist: $dirlist"
+	echo "|| sublist: $sublist"
+	echo "|| filelist: $filelist"
+	echo ""
+
+	## simple scans
 	print_header "Starting basic scans on ports and website dirs"
-	run_command "nmap basic tcp on $target"  "nmap -sCV -T4 -v $target -o nmap.scan"
-	if [ "$web_scans" = true ]; then
-		run_command "ffuf scanning for directories on the website $website" "ffuf -u http://$website/FUZZ -w $dirs_wordlist -o dirs.scan"
-	fi
+	# nmap scans
+	run_command "nmap basic tcp on $target"  "nmap -sCV -T4 -v $target -oN nmap.scan"
+	# ffuf scan
+	run_command "ffuf scanning for directories on the website $url" "ffuf -u http://$url/FUZZ -w $dirlist -o dirs.scan"
 
+	## advanced scans
 	print_header "Now running advanced scans on ports and website"
-	if [ "web_scans" = true ]; then
-		run_command "ffuf scanning for subdomains on the website $website" "ffuf -u http://FUZZ.$website -w $subs_wordlist -o subs.scan"
-		run_command "ffuf scanning for files on the website $website" "ffuf -u http://$website/FUZZ -w $files_wordlist -o files.scan"
-	fi
-	run_command "nmap all ports scan on $target" "nmap -T4 -v -p- --open $target -o nmap-all-ports.txt"
-	run_command "nmap udp scan on $target" "nmap -sU -v -T4 target -o nmap-udp.scan"
+	# ffuf scans
+	run_command "ffuf scanning for subdomains on the website $url" "ffuf -u http://FUZZ.$url -w $sublist -o subs.scan"
+	run_command "ffuf scanning for files on the website $url" "ffuf -u http://$url/FUZZ -w $filelist -o files.scan"
+	# nmap scans
+	run_command "nmap all ports scan on $target" "nmap -T4 -v -p- --open $target -oN nmap-all-ports.txt"
+	run_command "nmap udp scan on $target" "nmap -sU -v -T4 $target -oN nmap-udp.scan"
 
-fi
+}
+
+main "$@"
